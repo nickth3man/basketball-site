5d9e82405349c2451ffa8be627ba9724933926bc
Nicolas Alexander <nickth3man@gmail.com>
2026-03-04 21:09:07 -0500

    feat(queries): update index.ts to export all new query modules

--- nba-reference/src/lib/queries/index.ts
+++ nba-reference/src/lib/queries/index.ts
@@ -10,8 +10,18 @@ export * from './players';
 export * from './teams';
 export * from './games';
 export * from './seasons';
+export * from './leaders';
+export * from './draft';
+export * from './playoffs';
+export * from './awards';
+export * from './allstar';
+export * from './standings';
+export * from './team-schedule';
+export * from './player-splits';
+export * from './franchise';
+export * from './frivolities';
 
 // Re-export feature queries for convenience
 export { getHomeStandings, getRecentGames } from '../query/home';
 export { searchEntities } from '../query/search';
-export { getPlayerDirectory, getTeamDirectory } from '../query/directory';
+export { getPlayerDirectory, getPlayerDirectoryByLetter, getTeamDirectory } from '../query/directory';
